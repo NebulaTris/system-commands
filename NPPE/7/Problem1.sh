7684d56e7a08ffa037a8d43841bb30a3f689c07a
Shambhavi Mishra <94922914+NebulaTris@users.noreply.github.com>
2023-05-02 15:52:04 +0530

    Final

---
+++ NPPE/7/Problem1.sh
@@ -0,0 +1,11 @@
+: 'Write a bash script which takes one argument as the name of a file and prints Yes if the file has read 
+permission only for the owner and no other permissions for owner or other users, else do not print anything. 
+The file given in the argument will be present in the current working directory.
+'
+
+#Solution
+script() {
+if [[ $(ls -l $1 | grep -e "^-r--------.*") ]] ; then
+    echo "Yes"
+fi
+}
